b1237dd077fd7222e953586490bba4c9319b780c
SoniaBharathi17 <81243343+SoniaBharathi17@users.noreply.github.com>
2021-03-23 20:39:44 +0530

    ts file for login and register

---
+++ login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';    
+import { Router } from '@angular/router';    
+import { LoginService } from '../login.service';    
+import { FormsModule } from '@angular/forms';    
+import { NgModule  } from '@angular/core';
+    
+@Component({    
+  selector: 'app-login',    
+  templateUrl: './login.component.html',    
+  styleUrls: ['./login.component.css']    
+})    
+export class LoginComponent {    
+    
+  model : any={};    
+  errorMessage: any;
+    
+  
+  constructor(private router:Router,private LoginService:LoginService) { }    
+    
+    
+  ngOnInit() {    
+    sessionStorage.removeItem('UserName');    
+    sessionStorage.clear();    
+  }    
+  
+  login(){    
+    ;  
+    debugger;    
+    this.LoginService.Login(this.model).subscribe(    
+      data => {    
+        debugger;    
+        if(data.Status=="Success")    
+        {       
+          this.router.navigate(['/Dashboard']);    
+          debugger;    
+        }    
+        else{    
+          this.errorMessage = data.Message;    
+        }    
+      },    
+      error => {    
+        this.errorMessage = error.message;    
+      });    
+  };    
+ }     
